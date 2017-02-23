e10e6791d12048a18142f263acf98a74c2fe9bd4
And911 <andriy.kandyba@gmail.com>
2017-02-23 21:11:00 +0100

    Scorer for ResultFiles and Score computing
    prepareResultFile method is ready

---
+++ hashcode-01/src/main/java/Controller/Scorer.java
@@ -0,0 +1,58 @@
+package Controller;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.List;
+
+import model.CacheServer;
+import model.DataCenter;
+import model.Endpoint;
+import model.Video;
+
+public class Scorer {
+	
+	public void prepareResultFile(DataCenter result, String filename){
+		PrintWriter out = null;
+		try {
+		  out = new PrintWriter(new OutputStreamWriter(
+		      new BufferedOutputStream(new FileOutputStream(filename+".out"))));
+		  List<CacheServer> cacheServers = result.getCacheServers();
+		  int numberOfCacheServers = cacheServers.size();
+		  out.println(numberOfCacheServers);
+		  int i = 0;
+		  for (CacheServer cacheserver: cacheServers) {
+			  List<Video> videos = cacheserver.getCachedVideos().getVideos();
+			  String videoString = "";
+			  for (Video video : videos) {
+				videoString += " "+video.getId();
+			  }
+			  out.println(i+videoString);
+			  i++;
+		  }
+		} catch (FileNotFoundException e) {
+		  e.printStackTrace();
+		} finally {
+		  if(out != null) {
+		    out.flush();
+		    out.close();
+		  }
+		}
+
+	}
+
+	public Long computeScore(DataCenter resultSet){
+		List<CacheServer> cacheServers = resultSet.getCacheServers();
+		List<Endpoint> endpoints = resultSet.getEndpoints();
+		Long score = 0L;
+		
+		for (Endpoint endpoint : endpoints) {
+			int basicLatency = endpoint.getLatency();
+			
+		}
+		
+		return score;
+	}
+}
